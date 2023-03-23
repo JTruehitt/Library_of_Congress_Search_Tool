ebdce5df5164fae7ad3526fc9ca8004b8bc2d9ff
JTrue <truehitt@gmail.com>
2023-03-23 15:41:25 -0400

    made successful connection to API. need to explore more

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,38 @@
 let baseURL = "https://www.loc.gov/";
 let toJSON = "fo=json";
 
-let searchMethod = ["search", "collections", { format: "maps" }];
+let format = {
+  maps: "maps",
+  audio: "audio",
+  photos: "photos",
+  manuscripts: "manuscripts",
+  newspapers: "newspapers",
+  videos: "film-and-videos",
+  music: "notated-music",
+  websites: "websites",
+};
 
-function queryLOC() {}
+let searchMethod = ["search", "collections", format, "item"];
+
+function queryLOC() {
+  let requestURL = baseURL + "search/?q=baseball&" + toJSON;
+  console.log(requestURL);
+  fetch(requestURL)
+    .then((res) => {
+      if (!res.ok) {
+        console.log(res.status);
+      } else {
+        console.log(res);
+        return res.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+$(".searchBtn").click(function () {
+  searchText = $(".mainSearch").val();
+  console.log(searchText);
+  queryLOC();
+});
