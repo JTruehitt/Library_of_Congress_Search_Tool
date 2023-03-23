30e8f3b57981a49dcb328070be237bce5b14b846
JTrue <truehitt@gmail.com>
2023-03-22 20:17:03 -0400

    first commit

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// https://libraryofcongress.github.io/data-exploration/
+
+let baseURL = "https://www.loc.gov/";
+let toJSON = "fo=json";
+
+let searchMethod = ["search", "collections", { format: "maps" }];
+
+function queryLOC() {}
