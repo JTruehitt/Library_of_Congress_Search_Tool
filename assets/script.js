a9127be95dab168e3291ad1682d554a111eb6e23
JTrue <truehitt@gmail.com>
2023-03-24 15:37:37 -0400

    successfully pulled and parsed data from loc api and rendered to page in an organized format. need additional functionality to connect to separate results page

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,21 @@
 // https://libraryofcongress.github.io/data-exploration/
 
 let baseURL = "https://www.loc.gov/";
-let toJSON = "fo=json";
+let toJSON = "&fo=json";
 
-let format = {
-  maps: "maps",
-  audio: "audio",
-  photos: "photos",
-  manuscripts: "manuscripts",
-  newspapers: "newspapers",
-  videos: "film-and-videos",
-  music: "notated-music",
-  websites: "websites",
-};
+let searchText;
+let searchMethod;
 
-let searchMethod = ["search", "collections", format, "item"];
+let resultsContainer = $(".resultsContainer");
 
-function queryLOC() {
-  let requestURL = baseURL + "search/?q=baseball&" + toJSON;
+function queryLOC(searchMethod, searchText) {
+  let requestURL = baseURL + searchMethod + searchText + toJSON;
   console.log(requestURL);
   fetch(requestURL)
     .then((res) => {
       if (!res.ok) {
         console.log(res.status);
+        alert("Sorry, your request didn't go through as submitted.\n\nPlease adjust your search and try again.");
       } else {
         console.log(res);
         return res.json();
@@ -30,11 +23,62 @@ function queryLOC() {
     })
     .then((data) => {
       console.log(data);
+      displayResults(data);
     });
 }
 
-$(".searchBtn").click(function () {
-  searchText = $(".mainSearch").val();
-  console.log(searchText);
-  queryLOC();
+function displayResults(data) {
+resultsContainer.empty();
+
+for (let i = 0; i < data.results.length; i++) {
+  result = $("<div>");
+  resultIMG = $("<img>");
+  resultDesc = $("<p>");
+  resultInfoContainer = $("<div>");
+  resultTextContainer = $("<div>");
+  resultTitle = $("<h3>")
+  resultURL = data.results[i].url;
+  resultBtn = $("<a>");
+  resultIMGURL = data.results[i].image_url;
+
+  // fetch(resultIMGURL, {
+  //   mode: "no-cors"
+  // })
+  // .then(function(res) {
+  //   if (!res.ok) {
+  //     resultIMGURL = "https://plchldr.co/i/500x250"
+  //   }
+  // })
+
+  resultTitle.text(data.results[i].title)
+  resultBtn.text("Get More Info");
+  resultDesc.text(data.results[i].description)
+
+  resultIMG.attr("src", resultIMGURL);
+  resultInfoContainer.addClass("with-sidebar resultInfoContainer");
+  resultBtn.attr("href", resultURL);
+  resultBtn.addClass("resultBtn");
+  result.addClass("resultBox stack");
+  resultTextContainer.addClass("stack resultTextContainer");
+
+  resultInfoContainer.append(resultIMG);
+  resultInfoContainer.append(resultTextContainer);
+  resultTextContainer.append(resultDesc);
+  resultTextContainer.append(resultBtn);
+  result.append(resultTitle);
+  result.append(resultInfoContainer);
+  resultsContainer.append(result);
+}
+
+}
+
+$(".searchBtn").click(function (e) {
+  e.preventDefault()
+  
+  searchText = $(".mainSearch").val().toLowerCase().trim().replace(" ", "+");
+  searchMethod = $("#searchMethod").val();
+  console.log(searchMethod + searchText);
+  queryLOC(searchMethod, searchText);
+  // location.replace("results.html")
+  
 });
